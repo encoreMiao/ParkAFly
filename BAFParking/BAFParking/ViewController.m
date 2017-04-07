4bbb16eb961dd6883fc58f1828d88f213e5eb6c8
mengmeng <mengmeng@mengmengtekiMacBook-Pro.local>
2017-04-08 06:39:23 +0800

    Create Project

---
+++ BAFParking/BAFParking/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BAFParking
+//
+//  Created by mengmeng on 2017/4/7.
+//  Copyright © 2017年 boanfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
